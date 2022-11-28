e6786177b6f4f4a0bd658c594e98d30f479317fd
Shubham8144 <shubham32yadav@gmail.com>
2022-11-28 18:58:56 +0530

    Deleted the file from the git repository

--- subquery_basics/fuc.py
+++
@@ -1,5 +0,0 @@
-from django.db.models import F
-from subquery_basics.models import Category, Hero
-from subquery_basics.factories import CategoryFactory, HeroFactory
-cat1=Category.objects.first()
-cat2=Category.objects.last()
